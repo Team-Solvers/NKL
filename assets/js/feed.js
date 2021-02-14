1757707a7ac04aa620d63da048bf9056d339f9c4
Lingerew711 <se.lingerew.getie@gmail.com>
2021-02-14 14:25:50 -0800

    unnecessary comments removed

--- assets/js/feed.js
+++ assets/js/feed.js
@@ -1,11 +1,9 @@
-// AOS.init();
 const sidebar = document.querySelector('.sidebar')
 let _sidebarEnlarged = true
 const sidebarLink = document.querySelectorAll('.sidebar-links')
 const sidebarLinks = Array.from(sidebarLink)
 const arrowCollapse = document.querySelector(".arrow-collapse")
 const arrowCollapseSuggestion = document.querySelector(".arrow-collapse-suggestion")
-// const rightSidebar = document.querySelector(".right-sidebar")
 const pageWrapper = document.querySelector(".page-wrapper")
 const post = document.querySelector(".post")
 const postsWrapper = document.querySelector(".posts-wrapper")
@@ -40,15 +38,11 @@ arrowCollapseSuggestion.addEventListener("click", function (e) {
             topNavigation.style.width = "calc(100% - 4.2rem)"
             topNavigation.style["transition"]= ".2s all"
         }
-        // topNavigation.style.marginLeft = "10%"
         mainContent.style.marginLeft = "7%"
         mainContent.style["transition"]= ".2s all"
         topNavigation.style["transition"]= ".2s all"
         arrowCollapseSuggestion.style["transform"] = "rotate(0)"
         arrowCollapseSuggestion.style["transition"] = ".2s all"
-        // suggestionImage.style.width = "2rem"
-        // suggestionImage.style.height = "2rem"
-        
         Right_sidebarEnlarged = false
         suggestionHeader.style.fontSize = "0"
         suggestionHeader.style.transition = ".2s all"
@@ -75,44 +69,9 @@ arrowCollapseSuggestion.addEventListener("click", function (e) {
         Right_sidebarEnlarged = true
     }
 })
-// window.onresize = (e) => {
-//     // console.log("window.innerWidth");
-//     if (window.innerWidth <= 1575) {
-//         // console.log("yearherlhjljdhajl fpl;dsahf ");
-//         rightSidebar.style.display = "none"
-//         topNavigation.style.marginRight = "0"
-//         topNavigation.style.width = "calc(100% - 27px)"
-//         mainContent.style.marginRight = "0"
-//         mainContent.style.marginLeft = "27px"
-//     }
 
-//     if(window.innerWidth <= 1000) {
-//         StoryAvatars.forEach((e,i) => {
-//             e.style.width = "2rem"
-//             e.style.height = "2rem"
-//             addStoryGradient[i].style.width = "2rem"
-//             addStoryGradient[i].style.height = "2rem"
-//             storyName[i].style.fontSize = ".7rem"
-//         })
-//     }
-// }
-
-// window.addEventListener("DOMContentLoaded", (e) => {
-//     console.log("loaded");
-//     if (window.innerWidth <= 1561) {
-//         rightSidebar.style.display = "none"
-//         mainContent.style.marginRight = "0"
-//         mainContent.style.marginLeft = "27px"
-
-//         topNavigation.style.marginRight = "0"
-//         topNavigation.style.width = "calc(100% - 27px)"
-//         sidebar.style.zIndex = "23"
-//     }
-// })
 post.addEventListener("focus", function (e) {
 
-    // postsWrapper.style.marginTop = "300px"
-    // postsWrapper.style.transition = ".2s all"
     contentCad.style.marginBottom = "25rem"
     contentCad.style.transition = ".2s all"
 })
@@ -129,7 +88,6 @@ arrowCollapse.addEventListener("click", () => {
     if (!_sidebarEnlarged) {
         sidebar.style['width'] = "57px"
         sidebar.style['overflow'] = "hidden"
-        // $(".sidebar-links").hide();
         topNavigation.style["transition"]= ".2s all"
         if(rightSidebar.style["width"] == "30vh"){
             topNavigation.style.marginLeft = "57px"
@@ -140,7 +98,6 @@ arrowCollapse.addEventListener("click", () => {
             topNavigation.style.marginLeft = "57px"
             topNavigation.style.width = "calc(100% - 4.2rem - 57px)"
         }
-        // topNavigation.style.width = "calc(100%  - 20px)"
         sideBarAvatar.style.height = "1.8rem"
         sidebarAvatarName.style.fontSize = "0"
         sidebarAvatarName.style.marginTop = "0"
@@ -157,14 +114,6 @@ arrowCollapse.addEventListener("click", () => {
 
         sidebar.style["transition"] = "all .3s"
         _sidebarEnlarged = true
-
-        // if (window.innerWidth < 1400) {
-        //     topNavigation.style.marginLeft = "57px"
-        //     topNavigation.style.width = "calc(100%  - 27px)"
-        // } else {
-        //     topNavigation.style.marginLeft = "5%"
-        //     topNavigation.style.width = "calc(100%  - 30vh - 57px)"
-        // }
         topNavigation.style.transition = ".2s all"
     } else {
         if (window.innerWidth <= 1400) {
