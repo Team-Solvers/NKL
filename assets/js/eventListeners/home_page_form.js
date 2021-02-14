5721bc4f829c165ec94e68d9a7ec7c57b33476c7
par0s <se.kidus.yoseph@gmail.com>
2021-02-14 18:03:12 +0300

    [authentication] sign up validation including username check added

--- assets/js/eventListeners/home_page_form.js
+++ assets/js/eventListeners/home_page_form.js
@@ -1,4 +1,5 @@
 import {logIn} from "../controllers/authenticate.js";
+import {addUserToDB} from "../controllers/signUp.js";
 
 const logInForm = document.querySelector(".login-form");
 const logInFormUserNameInput = document.querySelector(".login-form-email");
@@ -13,6 +14,12 @@ const signUpEmail = document.querySelector(".e-mail");
 const signUpPassword = document.querySelector(".p-word");
 const signUpConfirmPassword = document.querySelector(".c-p-word");
 const signUpFormSubmitBtn = document.querySelector(".submit");
+
+const passwordValidation = document.querySelector(".signup-form-password-validation-feedback");
+const confirmValidation = document.querySelector(".signup-form-confirm-validation-feedback");
+const fullnameValidation = document.querySelector(".signup-form-fullname-validation-feedback");
+const emailValidation = document.querySelector(".signup-form-email-validation-feedback");
+const usernameValidation = document.querySelector(".signup-form-username-validation-feedback");
 
 signInBtn.addEventListener('click',authenticateUser);
 signUpFormSubmitBtn.addEventListener('click',addNewUserToDB);
@@ -32,10 +39,85 @@ async function authenticateUser(e){
     }
 }
 
-async function addNewUserToDB(e){
-    e.preventDefault()
-    console.log("registering");
+
+
+//check if username is taken - handled by db
+//make sure fullname is not empty
+//email check
+//change password input type to password
+
+async function addNewUserToDB(e){    
+    let fullName = signUpFullName.value;
+    let username = signUpUserName.value;
+    let userEmail = signUpEmail.value;
+    let password = signUpPassword.value;
+    let confirmPassword = signUpConfirmPassword.value;
+    
+    
+    let pc = passwordLength();
+    let cc = isConfirmPasswordSame();
+    let fc = isFullnameValid();
+    let ec = isEmailValid();
+    
+    if(pc && cc && fc && ec){        
+        let usernameTaken = await addUserToDB(username,fullName,userEmail,password);
+        if(usernameTaken === "username taken"){
+            usernameValidation.innerHTML = usernameTaken;
+        }
+        else{
+            usernameValidation.innerHTML = "";
+        }
+    }
+}
+
+function passwordLength(){
+    let length = 3;
+    let password = signUpPassword.value;
+    let passwordLengthValidationText = `Password length should be greater than ${length}`;
+
+    if(password.length < length){
+        passwordValidation.innerHTML = passwordLengthValidationText;
+        return false
+    }
+    passwordValidation.innerHTML = "";
+    return true
+}
+
+function isConfirmPasswordSame(){
+    let password = signUpPassword.value;
+    let confirm = signUpConfirmPassword.value;
+    let confirmValidationText = `Password doesn't match`;
+
+    if(passwordLength()){        
+        if(password !== confirm){
+            confirmValidation.innerHTML = confirmValidationText;
+            return false
+        }
+        confirmValidation.innerHTML = "";
+        return true
+    }
+    return false;
 }
 
 
+function isFullnameValid(){
+    let fullname = signUpFullName.value;
+    let fullnameValidationText = "Name can't be empty";
+    if(fullname.length == 0){
+        fullnameValidation.innerHTML = fullnameValidationText;
+        return false
+    }
+    fullnameValidation.innerHTML = "";
+    return true;
+}
 
+function isEmailValid(){
+    let email = signUpEmail.value;
+    let emailValidationText = "Invalid email";
+    if(email.indexOf("@") == -1){
+        emailValidation.innerHTML = emailValidationText;
+        return false
+    }
+    emailValidation.innerHTML = "";
+    return true;
+}
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,7 @@
 document.querySelector('#sign-up-modal').addEventListener('click', function () {
     document.querySelector('.overlay').classList.add('d-block');
     document.querySelector('.modal-card').classList.add('d-block');
-    console.log("Triggered");
+    // console.log("Triggered");
 });
 
 // document.getElementById('done-sign-up').addEventListener('click', function () {
@@ -12,5 +12,5 @@ document.querySelector('#sign-up-modal').addEventListener('click', function () {
 document.querySelector('.overlay').addEventListener('click', function () {
     document.querySelector('.overlay').classList.remove('d-block');
     document.querySelector('.modal-card').classList.remove('d-block');
-    console.log("clicked");
+    // console.log("clicked");
 });
