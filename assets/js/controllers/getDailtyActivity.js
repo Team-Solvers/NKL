6a42b1ed9d041b99c311cb515f83a6bd8c07487c
Natnael Abay <se.natnael.abay@gmail.com>
2021-03-04 01:18:18 +0300

    [graph] activity graph setup

--- assets/js/controllers/getDailtyActivity.js
+++ assets/js/controllers/getDailtyActivity.js
@@ -21,7 +21,7 @@ export async function getDailtyActivity(user_id){
         activityHistory.push([date,dateMap.get(date)]);
     }
     
-    console.log(activityHistory);
+    // console.log(activityHistory);
     return activityHistory;
 }
 
